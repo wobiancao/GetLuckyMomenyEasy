dcb2be778ed3e4c8e46a8c2f46321fe02432528f
xiaoyu <420245103@qq.com>
2016-01-26 16:14:11 +0800

    qq发送bug 微信和qq根据包来分隔

--- app/src/main/java/com/wobiancao/getluckymomenyeasy/service/HouSaiLeiService.java
+++ app/src/main/java/com/wobiancao/getluckymomenyeasy/service/HouSaiLeiService.java
@@ -6,8 +6,11 @@ import android.app.Notification;
 import android.app.PendingIntent;
 import android.os.Build;
 import android.os.Parcelable;
+import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityNodeInfo;
+
+import com.wobiancao.getluckymomenyeasy.utils.L;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,6 +19,12 @@ import java.util.List;
  * Created by xy on 16/1/26.
  */
 public class HouSaiLeiService extends AccessibilityService {
+    private final String PACKAGENAME_QQ = "com.tencent.mobileqq";
+    private final String PACKAGENAME_WECAHT = "com.tencent.mm";
+    private final String TAG = "HouSaiLeiService";
+    private List<AccessibilityNodeInfo> mReceiveNode;
+    private boolean isQQ = false;//是否是qq
+    private boolean isWeChat = false;//是否是微信
     //qq
     private static final String QQ_OPEN_EN = "Open";
     private static final String QQ_OPENED_EN = "You've opened";
@@ -29,7 +38,6 @@ public class HouSaiLeiService extends AccessibilityService {
     private long lastFetchedTime = 0;
     private static final int MAX_CACHE_TOLERANCE = 5000;
     private AccessibilityNodeInfo rootNodeInfo;
-    private List<AccessibilityNodeInfo> mReceiveNode;
     //wechat
     private static final String WECHAT_DETAILS_EN = "Details";
     private static final String WECHAT_DETAILS_CH = "红包详情";
@@ -41,10 +49,10 @@ public class HouSaiLeiService extends AccessibilityService {
     private final static String WECHAT_NOTIFICATION_TIP = "[微信红包]";
     private boolean mMutex = false;
 
-    private AccessibilityNodeInfo  mUnpackNode;
+    private AccessibilityNodeInfo mUnpackNode;
     private boolean mLuckyMoneyPicked, mNeedUnpack, mNeedBack;
     private String lastContentDescription = "";
-    private HongbaoSignature signature = new HongbaoSignature();
+
     @TargetApi(Build.VERSION_CODES.KITKAT)
     public void recycle(AccessibilityNodeInfo info) {
         if (info.getChildCount() == 0) {
@@ -74,66 +82,58 @@ public class HouSaiLeiService extends AccessibilityService {
             if (watchNotifications(event)) {
                 return;
             }
-            if (watchList(event)){
+            if (watchList(event)) {
                 return;
             }
         }
         mReceiveNode = null;
         checkNodeInfo();
-        /* 如果已经接收到红包并且还没有戳开 */
-        if (mLuckyMoneyReceived && (mReceiveNode != null)) {
-            mMutex = true;
-            int size = mReceiveNode.size();
-            if (size > 0) {
-                String id = getHongbaoText(mReceiveNode.get(size - 1));
-                long now = System.currentTimeMillis();
-                if (this.shouldReturn(id, now - lastFetchedTime)){
-                    return;
-                }
-                lastFetchedHongbaoId = id;
-                lastFetchedTime = now;
-                AccessibilityNodeInfo cellNode = mReceiveNode.get(size - 1);
-                if (cellNode.getText().toString().equals(QQ_HONG_BAO_PASSWORD_OPENED)) {
-                    return;
-                }
-                cellNode.getParent().performAction(AccessibilityNodeInfo.ACTION_CLICK);
-                if (cellNode.getText().toString().equals(QQ_HONG_BAO_PASSWORD)) {
-                    AccessibilityNodeInfo rowNode = getRootInActiveWindow();
-                    if (rowNode == null) {
-                        return;
-                    } else {
-                        recycle(rowNode);
-                    }
-                }
-                if (cellNode.getText() != null && cellNode.getText().toString().equals(QQ_CLICK_TO_PASTE_PASSWORD)) {
-                    cellNode.getParent().performAction(AccessibilityNodeInfo.ACTION_CLICK);
-                }
-                if (cellNode.getClassName().toString().equals("android.widget.Button") && cellNode.getText().toString().equals("发送")) {
-                    cellNode.performAction(AccessibilityNodeInfo.ACTION_CLICK);
-                }
-                mLuckyMoneyReceived = false;
-                mLuckyMoneyPicked = true;
-            }
-        }
-        /* 如果戳开但还未领取 */
-        if (mNeedUnpack && (mUnpackNode != null)) {
-            AccessibilityNodeInfo cellNode = mUnpackNode;
-            cellNode.performAction(AccessibilityNodeInfo.ACTION_CLICK);
-            if (cellNode.getText().toString().equals(QQ_HONG_BAO_PASSWORD)) {
-                AccessibilityNodeInfo rowNode = getRootInActiveWindow();
-                if (rowNode == null) {
-                    return;
-                } else {
-                    recycle(rowNode);
-                }
-            }
-            mNeedUnpack = false;
-        }
-        if (mNeedBack) {
-            performGlobalAction(GLOBAL_ACTION_BACK);
-            mMutex = false;
-            mNeedBack = false;
-        }
+           /* 如果已经接收到红包并且还没有戳开 */
+           if (mLuckyMoneyReceived && (mReceiveNode != null)) {
+               int size = mReceiveNode.size();
+               if (size > 0) {
+                   String id = getHongbaoText(mReceiveNode.get(size - 1));
+                   long now = System.currentTimeMillis();
+                   if (this.shouldReturn(id, now - lastFetchedTime)){
+                       return;
+                   }
+                   lastFetchedHongbaoId = id;
+                   lastFetchedTime = now;
+                   AccessibilityNodeInfo cellNode = mReceiveNode.get(size - 1);
+                   if (isQQ){
+                       if (cellNode.getText().toString().equals(QQ_HONG_BAO_PASSWORD_OPENED)) {
+                           return;
+                       }
+                   }
+                   cellNode.getParent().performAction(AccessibilityNodeInfo.ACTION_CLICK);
+                   if (isQQ){
+                       if (cellNode.getText().toString().equals(QQ_HONG_BAO_PASSWORD)) {
+                           AccessibilityNodeInfo rowNode = getRootInActiveWindow();
+                           if (rowNode == null) {
+                               L.e(TAG, "noteInfo is null");
+                               return;
+                           } else {
+                               recycle(rowNode);
+                           }
+                       }
+                   }
+                   mLuckyMoneyReceived = false;
+                   mLuckyMoneyPicked = true;
+               }
+           }
+             /* 如果戳开但还未领取 */
+           if (mNeedUnpack && (mUnpackNode != null)) {
+               AccessibilityNodeInfo cellNode = mUnpackNode;
+               cellNode.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+               mNeedUnpack = false;
+           }
+           if (mNeedBack) {
+               performGlobalAction(GLOBAL_ACTION_BACK);
+               mMutex = false;
+               mNeedBack = false;
+           }
+
+
 
     }
 
@@ -146,39 +146,58 @@ public class HouSaiLeiService extends AccessibilityService {
         if (rootNodeInfo == null) {
             return;
         }
+        L.e(TAG, rootNodeInfo.getPackageName().toString());
+        if (PACKAGENAME_QQ.equals(rootNodeInfo.getPackageName().toString())) {
+            isQQ = true;
+        }
+        if (PACKAGENAME_WECAHT.equals(rootNodeInfo.getPackageName().toString())) {
+            isWeChat = true;
+        }
+        if (isQQ) {
+             /* 聊天会话窗口，遍历节点匹配“点击拆开”，“口令红包”，“点击输入口令” */
+            List<AccessibilityNodeInfo> qqNodes1 = this.findAccessibilityNodeInfosByTexts(this.rootNodeInfo, new String[]{
+                    QQ_DEFAULT_CLICK_OPEN, QQ_HONG_BAO_PASSWORD, QQ_CLICK_TO_PASTE_PASSWORD, "发送"});
 
-         /* 聊天会话窗口，遍历节点匹配“点击拆开”，“口令红包”，“点击输入口令” */
-        List<AccessibilityNodeInfo> nodes1 = this.findAccessibilityNodeInfosByTexts(this.rootNodeInfo, new String[]{
-                QQ_DEFAULT_CLICK_OPEN, QQ_HONG_BAO_PASSWORD, QQ_CLICK_TO_PASTE_PASSWORD, WECHAT_VIEW_OTHERS_CH, WECHAT_VIEW_SELF_CH, "发送"});
-
-        if (!nodes1.isEmpty()) {
-            AccessibilityNodeInfo targetNode = nodes1.get(nodes1.size() - 1);
-//            String nodeId = Integer.toHexString(System.identityHashCode(this.rootNodeInfo));
-            if (this.signature.generateSignature(targetNode)) {
-                mLuckyMoneyReceived = true;
-                mReceiveNode = nodes1;
+            if (!qqNodes1.isEmpty()) {
+                String nodeId = Integer.toHexString(System.identityHashCode(this.rootNodeInfo));
+                if (!nodeId.equals(lastFetchedHongbaoId)) {
+                    mLuckyMoneyReceived = true;
+                    mReceiveNode = qqNodes1;
+                }
+                return;
             }
-            return;
         }
+        if (isWeChat) {
+             /* 聊天会话窗口，遍历节点匹配“领取红包”和"查看红包" */
+            List<AccessibilityNodeInfo> weChatnodes1 = this.findAccessibilityNodeInfosByTexts(this.rootNodeInfo, new String[]{
+                    WECHAT_VIEW_OTHERS_CH, WECHAT_VIEW_SELF_CH});
+            if (!weChatnodes1.isEmpty()) {
+                String nodeId = Integer.toHexString(System.identityHashCode(this.rootNodeInfo));
+                if (!nodeId.equals(lastFetchedHongbaoId)) {
+                    mLuckyMoneyReceived = true;
+                    mReceiveNode = weChatnodes1;
+                }
+                return;
+            }
         /* 戳开红包，红包还没抢完，遍历节点匹配“拆红包” */
-        AccessibilityNodeInfo node2 = (this.rootNodeInfo.getChildCount() > 3) ? this.rootNodeInfo.getChild(3) : null;
-        if (node2 != null && node2.getClassName().equals("android.widget.Button")) {
-            mUnpackNode = node2;
-            mNeedUnpack = true;
-            return;
-        }
+            AccessibilityNodeInfo weChatnode2 = (this.rootNodeInfo.getChildCount() > 3) ? this.rootNodeInfo.getChild(3) : null;
+            if (weChatnode2 != null && weChatnode2.getClassName().equals("android.widget.Button")) {
+                mUnpackNode = weChatnode2;
+                mNeedUnpack = true;
+                return;
+            }
 
         /* 戳开红包，红包已被抢完，遍历节点匹配“红包详情”和“手慢了” */
-        if (mLuckyMoneyPicked) {
-            List<AccessibilityNodeInfo> nodes3 = this.findAccessibilityNodeInfosByTexts(this.rootNodeInfo, new String[]{
-                    WECHAT_BETTER_LUCK_CH, WECHAT_DETAILS_CH,
-                    WECHAT_BETTER_LUCK_EN, WECHAT_DETAILS_EN, WECHAT_EXPIRES_CH});
-            if (!nodes3.isEmpty()) {
-                mNeedBack = true;
-                mLuckyMoneyPicked = false;
+            if (mLuckyMoneyPicked) {
+                List<AccessibilityNodeInfo> weChatnodes3 = this.findAccessibilityNodeInfosByTexts(this.rootNodeInfo, new String[]{
+                        WECHAT_BETTER_LUCK_CH, WECHAT_DETAILS_CH,
+                        WECHAT_BETTER_LUCK_EN, WECHAT_DETAILS_EN, WECHAT_EXPIRES_CH});
+                if (!weChatnodes3.isEmpty()) {
+                    mNeedBack = true;
+                    mLuckyMoneyPicked = false;
+                }
             }
         }
-
     }
 
 
@@ -214,8 +233,9 @@ public class HouSaiLeiService extends AccessibilityService {
      */
     private boolean shouldReturn(String id, long duration) {
         // ID为空
-        if (id == null) return true;
-
+        if (id == null) {
+            return true;
+        }
         // 名称和缓存不一致
         if (duration < MAX_CACHE_TOLERANCE && id.equals(lastFetchedHongbaoId)) {
             return true;
@@ -239,9 +259,6 @@ public class HouSaiLeiService extends AccessibilityService {
             List<AccessibilityNodeInfo> nodes = nodeInfo.findAccessibilityNodeInfosByText(text);
 
             if (!nodes.isEmpty()) {
-                if (text.equals(QQ_OPEN_EN) && !nodeInfo.findAccessibilityNodeInfosByText(QQ_OPENED_EN).isEmpty()) {
-                    continue;
-                }
                 return nodes;
             }
         }
@@ -252,6 +269,7 @@ public class HouSaiLeiService extends AccessibilityService {
     public void onInterrupt() {
 
     }
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     private boolean watchList(AccessibilityEvent event) {
         // Not a message
         if (event.getEventType() != AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED || event.getSource() == null)
@@ -269,28 +287,35 @@ public class HouSaiLeiService extends AccessibilityService {
         }
         return false;
     }
-
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     private boolean watchNotifications(AccessibilityEvent event) {
+        final int eventType = event.getEventType();
         // Not a notification
-        if (event.getEventType() != AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED) {
+        if (eventType != AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED) {
             return false;
         }
 
         // Not a hongbao
-        String tip = event.getText().toString();
-        if (!tip.contains(WECHAT_NOTIFICATION_TIP) && tip.contains(QQ_NOTIFICATION_TIP)){
-            return true;
-        }
+        List<CharSequence> tips = event.getText();
+        if(!tips.isEmpty()) {
+            for(CharSequence t : tips) {
+                String tip = String.valueOf(t);
+                if(tip.contains(WECHAT_NOTIFICATION_TIP) || tip.contains(QQ_NOTIFICATION_TIP)) {
+                    Parcelable parcelable = event.getParcelableData();
+                    if (parcelable instanceof Notification) {
+                        Notification notification = (Notification) parcelable;
+                        try {
+                            notification.contentIntent.send();
+                            return true;
+                        } catch (PendingIntent.CanceledException e) {
+                            e.printStackTrace();
+                        }
+                    }
 
-        Parcelable parcelable = event.getParcelableData();
-        if (parcelable instanceof Notification) {
-            Notification notification = (Notification) parcelable;
-            try {
-                notification.contentIntent.send();
-            } catch (PendingIntent.CanceledException e) {
-                e.printStackTrace();
+                }
             }
         }
-        return true;
+        return false;
     }
+
 }
---
+++ app/src/main/java/com/wobiancao/getluckymomenyeasy/utils/L.java
@@ -0,0 +1,77 @@
+package com.wobiancao.getluckymomenyeasy.utils;
+
+import android.util.Log;
+
+/**
+ * Log统一管理类
+ *
+ *
+ */
+public class L {
+	public static boolean isDebug = true;// 是否需要打印bug，
+	private static final String TAG = "sage";
+
+	// 下面四个是默认tag的函数
+
+	public static void i(String msg) {
+		if (isDebug)
+			Log.i(TAG, msg);
+	}
+
+	public static void d(String msg) {
+		if (isDebug)
+			Log.d(TAG, msg);
+	}
+
+	public static void e(String msg) {
+		if (isDebug)
+			Log.e(TAG, msg);
+	}
+
+	public static void v(String msg) {
+		if (isDebug)
+			Log.v(TAG, msg);
+	}
+
+	// 下面是传入类名打印log
+	public static void i(Class<?> _class, String msg) {
+		if (isDebug)
+			Log.e(_class.getName(), msg);
+	}
+
+	public static void d(Class<?> _class, String msg) {
+		if (isDebug)
+			Log.e(_class.getName(), msg);
+	}
+
+	public static void e(Class<?> _class, String msg) {
+		if (isDebug)
+			Log.e(_class.getName(), msg + " ");
+	}
+
+	public static void v(Class<?> _class, String msg) {
+		if (isDebug)
+			Log.e(_class.getName(), msg);
+	}
+
+	// 下面是传入自定义tag的函数
+	public static void i(String tag, String msg) {
+		if (isDebug)
+			Log.e(tag, msg + " ");
+	}
+
+	public static void d(String tag, String msg) {
+		if (isDebug)
+			Log.e(tag, msg + " ");
+	}
+
+	public static void e(String tag, String msg) {
+		if (isDebug)
+			Log.e(tag, msg + " ");
+	}
+
+	public static void v(String tag, String msg) {
+		if (isDebug)
+			Log.e(tag, msg + " ");
+	}
+}
